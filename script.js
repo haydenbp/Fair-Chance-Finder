d3a08944bdc7cf50bf5b58556af59f58c1e059f2
haydenbp <32523980+haydenbp@users.noreply.github.com>
2021-03-16 17:27:40 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const locationDiv = document.getElementsByClassName('location accessible-contrast-color-location')
+const companyDiv = document.getElementsByClassName('company')
+
+
+let locations = []
+let companies = []
+let output = ''
+
+companyDiv.forEach(element => companies.push(element.innerText));
+locationDiv.forEach(element => addresses.push(element.innerText));
+
+for(let i = 0; i < companies.length; i++){
+
+    output +=`${companies[i]};${addresses[i]}\n`
+
+
+
+}
+
+prompt('here you go',output)
+
